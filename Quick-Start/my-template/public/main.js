fcc23993c2a14bc90a559ad69bbe08604b736f5a
Austin Kaylor <AustinJKaylor@eaton.com>
2024-08-22 20:26:09 -0400

    Making a custom template with main.js and main.css

---
+++ Quick-Start/my-template/public/main.js
@@ -0,0 +1,16 @@
+﻿// This is a basic setup that changes the default color mode to dark and adds some icon links in the header
+export default {
+    defaultTheme: 'dark',
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/dotnet/docfx',
+            title: 'GitHub'
+        },
+        {
+            icon: 'twitter',
+            href: 'https://twitter.com',
+            title: 'Twitter'
+        }
+    ]
+}
